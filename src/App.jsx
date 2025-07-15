58897e7466efeecf17e790643df2f077fb3ff737
Anish <anishshendarkar@gmail.com>
2025-07-15 12:10:47 +0530

    implemented first part of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className='text-4xl font-bold mt-10'>GSAP with React and Tailwind CSS</h1>
-    </div>
+    <main>
+        <Navbar />
+        <Hero />
+        <div className='h-dvh bg-black'/>
+    </main>
   )
 }
 
